d1182d5c31b27b245ed4f4f1bf69aa06bfd3b12f
smgd <soapmangoesdown@gmail.com>
2020-01-07 05:02:43 +0300

    add dummy log out button

--- src/app/components/dashboard/Dashboard.jsx
+++ src/app/components/dashboard/Dashboard.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { clearAuthToken } from "../../api/api";
+import history from "../../router/history";
+import { LoginContext } from "../authentication/LoginContext";
 
-const Dashboard = (props) => {
-  const { user } = props;
+const Dashboard = () => {
+  const { user, setUser } = useContext(LoginContext);
   return (
     <div>
       hi, {user.username}!
+      <button
+        onClick={() => {
+          clearAuthToken();
+          setUser(() => ({
+            username: null,
+            isFetched: true,
+          }));
+          history.push('/');
+        }}
+      >
+        log out
+      </button>
     </div>
 
   )
