d70306c5de54474bba6516a9e97e0ccfe191b4c9
smgd <soapmangoesdown@gmail.com>
2020-01-07 16:39:39 +0300

    add dummy dashboard style

--- src/app/components/dashboard/Dashboard.jsx
+++ src/app/components/dashboard/Dashboard.jsx
@@ -2,26 +2,29 @@ import React, {useContext} from 'react';
 import { clearAuthToken } from "../../api/api";
 import history from "../../router/history";
 import { LoginContext } from "../authentication/LoginContext";
+import { Container, WhiteCard } from "../common/Common.styles";
+import Button from "../common/Buttons/Button";
 
 const Dashboard = () => {
   const { user, setUser } = useContext(LoginContext);
   return (
-    <div>
-      hi, {user.username}!
-      <button
-        onClick={() => {
-          clearAuthToken();
-          setUser(() => ({
-            username: null,
-            isFetched: true,
-          }));
-          history.push('/');
-        }}
-      >
-        log out
-      </button>
-    </div>
-
+    <Container>
+      <WhiteCard>
+        <h2>Hi, {user.username}!</h2>
+        <Button
+          title="Log Out"
+          type="dark"
+          onClick={() => {
+            clearAuthToken();
+            setUser(() => ({
+              username: null,
+              isFetched: true,
+            }));
+            history.push('/');
+          }}
+        />
+      </WhiteCard>
+    </Container>
   )
 };
 
