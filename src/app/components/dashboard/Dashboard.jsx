3531545412e411a21624853836fbfccf68fa7baf
smgd <soapmangoesdown@gmail.com>
2020-01-08 04:43:05 +0300

    remove bla bla

--- src/app/components/dashboard/Dashboard.jsx
+++ src/app/components/dashboard/Dashboard.jsx
@@ -15,8 +15,6 @@ const Dashboard = () => {
           Your name: {user.firstName} {user.lastName}
           <br/>
           Your email: {user.email}
-          <br/>
-          Bla bla
         </p>
         <Button
           title="Log Out"
