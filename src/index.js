1184d110da685a0e312a8058b374c0656dbe77df
smgd <soapmangoesdown@gmail.com>
2020-02-02 15:26:29 +0300

    add sentry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({dsn: "https://c9f2cd47a29d4260ae1c2295d21030db@sentry.io/2179662"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
