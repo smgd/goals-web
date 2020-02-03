9caea369e2b969aed398bec6448a7eeb76de9875
smgd <soapmangoesdown@gmail.com>
2020-02-03 14:41:20 +0300

    add interactive title for pie

--- src/app/components/common/Pie.jsx
+++ src/app/components/common/Pie.jsx
@@ -1,36 +1,80 @@
 import styled from "styled-components";
-import React from "react";
+import React, {useState} from "react";
 import ReactMinimalPieChart from "react-minimal-pie-chart";
 
 
-const PieWrapper = styled.div`
-  height: 40%;
-  width: 40%;
+const PieWithTitleWrapper = styled.div`
+  display: flex;
 `;
 
-const colors = [
-  '#36877F',
-  '#CC001F',
-  '#BCE3BE',
-  '#F2930C',
-  '#0CBCF2',
-];
+const PieWrapper = styled.div`
+  width: 40%;
+  border-radius: 50%;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+`;
+
+const Title = styled.h1`
+  color: #FF9091;
+`;
+
 
 const Pie = ({ areas, animate }) => {
+  const colors = [
+    ['#36877F', '#5f877f'],
+    ['#CC001F', '#c96877'],
+    ['#BCE3BE', '#d9e0da'],
+    ['#F2930C', '#efd5b1'],
+    ['#0CBCF2', '#bbe1ed'],
+  ];
+
+  const defaultPieData = areas.map((area, i) => ({
+    title: area.name,
+    value: area.weight,
+    color: colors[i][0],
+    key: area.id,
+  }));
+
+  const [ pieData, setPieData ] = useState(defaultPieData);
+  const [ title, setTitle ] = useState(null);
+
+
+  const onMouseOver = (e, props, index) => {
+    setPieData(prevState => prevState.map((datum, i) => {
+      if (i !== index) {
+        return {
+          ...datum,
+          color: colors[i][1],
+        };
+      }
+
+      return datum;
+    }));
+    setTitle(props[index].title);
+  };
+
+  const onMouseOut = () => {
+    setPieData(defaultPieData);
+    setTitle(null);
+  };
+
   return (
-    <PieWrapper>
-      <ReactMinimalPieChart
-        data={areas.map((area, i) => ({
-          title: area.name,
-          value: area.weight,
-          color: colors[i],
-          key: area.id,
-        }))}
-        segmentsStyle={{ cursor: 'pointer' }}
-        onClick={(e, data, i) => console.log(data, i)} // here will be link to area
-        animate={animate}
-      />
-    </PieWrapper>
+    <PieWithTitleWrapper>
+      <PieWrapper>
+        <ReactMinimalPieChart
+          data={pieData}
+          segmentsStyle={{ cursor: 'pointer', transition: 'stroke .3s' }}
+          onClick={(e, data, i) => console.log(data, i)} // here will be link to area
+          animate={animate}
+          onMouseOver={onMouseOver}
+          onMouseOut={onMouseOut}
+        />
+      </PieWrapper>
+      {title !== null &&
+        <Title>
+          {title}
+        </Title>
+      }
+    </PieWithTitleWrapper>
   )
 };
 
