68e2b70fb8a2beea11925c21121f0ad22679e111
smgd <soapmangoesdown@gmail.com>
2020-02-05 18:30:59 +0300

    add button shadow (for fun)

--- src/app/components/common/Buttons/Button.styles.jsx
+++ src/app/components/common/Buttons/Button.styles.jsx
@@ -6,10 +6,15 @@ const ButtonWrapper = styled('button')`
   border-radius: 2px;
   color: ${(props) => props.theme.font};
   border: ${(props) => props.theme.border};
-  box-shadow: 0 4px 4px #F3F3F3;
   background: ${(props) => props.theme.background};
   cursor: pointer;
   padding: 0;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+  display: inline-block;
+  &:hover {
+    box-shadow: 0 14px 18px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
 `;
 
 const Title = styled('div')`
