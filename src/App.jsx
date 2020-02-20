99a4732ddec14485594a6423c0e1ba5c40e5bb9c
Egor Rudinskiy <rudgv2008@gmail.com>
2020-02-20 20:15:57 +0300

    MergedContextProvider component
    Но может мы все таки будет использовать нормальный стэйт мэнеджмент? redux? mobx? effector?

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import { LoginProvider } from './app/components/authentication/LoginContext'
 import { ModalProvider } from './app/components/common/Modals/ModalContext'
 import { Container } from './app/components/common/Common.styles'
 import LocalizationProvider from './app/components/core/Internalization/LocalizationProvider'
+import MergedContextProvider from './app/components/core/MergedContextProvider'
+
+
+const PROVIDERS = [
+  LocalizationProvider,
+  LoginProvider,
+  ModalProvider,
+]
 
 const App = () => (
-  <LocalizationProvider>
-    <LoginProvider>
-      <ModalProvider>
-        <Container>
-          <AuthRouter />
-        </Container>
-      </ModalProvider>
-    </LoginProvider>
-  </LocalizationProvider>
+  <MergedContextProvider contextProviders={PROVIDERS}>
+    <Container>
+      <AuthRouter />
+    </Container>
+  </MergedContextProvider>
 )
 
 export default App
